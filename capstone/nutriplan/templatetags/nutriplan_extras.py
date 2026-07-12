9a191bfbd1fd9313b674c7acaccf6614ffba3a6c
AntonAzer <tonyazer86@gmail.com>
2026-07-12 16:58:11 +0300

    Delete capstone directory

--- capstone/nutriplan/templatetags/nutriplan_extras.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def get_item(dictionary, key):
-    """Allow dict[key] lookups in Django templates: {{ mydict|get_item:key }}"""
-    return dictionary.get(key)
